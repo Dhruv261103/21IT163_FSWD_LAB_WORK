9e0d688fe69da49f9901b58d31b1ebb11a41c383
Tanti Dhruv Balubhai <98478988+Dhruv261103@users.noreply.github.com>
2023-07-10 20:33:37 +0530

    Add files via upload

---
+++ WEEK-1/Task-1.js
@@ -0,0 +1,17 @@
+// var
+var var_x = 10;
+document.write("Original value of var_x (var):", var_x + "<br>"); 
+var_x = 20;
+document.write(" New value of var_x (var):", var_x + "<br>"); 
+
+// let
+let let_x = "Hello";
+document.write("Value of let_x (let):", let_x + "<br>"); 
+let_x = "World";
+document.write("New value of let_x (let):", let_x + "<br>"); 
+
+// const
+const const_x = [1, 2, 3];
+document.write("Value of const_x (const):", const_x + "<br>"); 
+//const_x = [4, 5, 6];    // give error besause we can not assign new value to const varriable
+document.write("New value of const_x (const):", const_x + "<br>");
